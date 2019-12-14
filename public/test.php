5aefa1fd7a5fd2ed3cf7f85220b340f6e32b8369
FoxxenDev <32935159+FoxxenDev@users.noreply.github.com>
2019-12-14 22:29:51 +0100

    Delete test.php

--- public/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-$data = array("content" => "[Civil] souhaite prendre un rendez-vous pour le [date] : [lien de la demande]");
-$curl = curl_init("https://discordapp.com/api/webhooks/655488252001910785/e-NV2e5RkawNuvX6xTXqt0mkooPvz2lK9yDIOV0VftieXBtMXZgW2HY4W0p7SzeBRSfw");
-curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-return curl_exec($curl);
